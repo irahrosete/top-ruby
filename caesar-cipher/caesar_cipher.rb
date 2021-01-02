940c7f545440b57a4a4cae52ef587c59089ae282
irah rosete <irah.rosete@outlook.com>
2021-01-02 15:57:10 +1100

    translate to ascii using regex

--- caesar-cipher/caesar_cipher.rb
+++ caesar-cipher/caesar_cipher.rb
@@ -1,5 +1,13 @@
 def caesar_cipher(string, num)
   arr = string.split("")
+  final_cipher = arr.map {|ch|
+  if ch.match?(/[a-zA-Z]/) == true
+    ch.ord + num
+  else
+    ch
+  end
+  }
+  final_cipher.map(&:chr).join
 end
 
 puts caesar_cipher("Hello", 5)
