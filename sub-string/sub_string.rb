be72c1a96b9671be65b1d395cae57dc7cac7510f
irah rosete <irah.rosete@outlook.com>
2021-01-03 20:11:20 +1100

    remove one count from remaining word-count pair

--- sub-string/sub_string.rb
+++ sub-string/sub_string.rb
@@ -3,8 +3,8 @@ array = ["below","down","go","going","horn","how","howdy","it","i","low","own","
 def substrings(string, dictionary)
   arr = string.gsub(/[^a-zA-Z\s]/, "").downcase.split(" ") + dictionary
   ans = arr.each_with_object(Hash.new(0)) {|word, count| count[word] += 1}
-  p ans.reject {|word, count| count <= 1}
-
+  f_ans = ans.reject {|word, count| count <= 1}
+  p f_ans.transform_values! {|count| count - 1}
 end
 
-substrings("BElow. down", array)
+substrings("BElow. down going going horn", array)
