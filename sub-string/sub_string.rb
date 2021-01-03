4cfe53ac3dcff1c0bfd660e62054e5907cbef4dc
irah rosete <irah.rosete@outlook.com>
2021-01-03 19:37:40 +1100

    add method to remove count from second argument array

--- sub-string/sub_string.rb
+++ sub-string/sub_string.rb
@@ -2,7 +2,9 @@ array = ["below","down","go","going","horn","how","howdy","it","i","low","own","
 
 def substrings(string, dictionary)
   arr = string.gsub(/[^a-zA-Z\s]/, "").downcase.split(" ") + dictionary
-  p arr.each_with_object(Hash.new(0)) {|word, count| count[word] += 1}
+  ans = arr.each_with_object(Hash.new(0)) {|word, count| count[word] += 1}
+  p ans.reject {|word, count| count <= 1}
+
 end
 
 substrings("BElow. down", array)
