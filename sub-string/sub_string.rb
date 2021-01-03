3fe72d73e04e5291fdb4af4db9463a0f8510cd7e
irah rosete <irah.rosete@outlook.com>
2021-01-03 19:33:57 +1100

    add method to count elements of both arrays

--- sub-string/sub_string.rb
+++ sub-string/sub_string.rb
@@ -1,3 +1,8 @@
-def substrings
+array = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
+def substrings(string, dictionary)
+  arr = string.gsub(/[^a-zA-Z\s]/, "").downcase.split(" ") + dictionary
+  p arr.each_with_object(Hash.new(0)) {|word, count| count[word] += 1}
 end
+
+substrings("BElow. down", array)
