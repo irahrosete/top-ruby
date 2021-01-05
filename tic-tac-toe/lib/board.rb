e9f85f4ab0c49b84a954adfe362ed82bdd03931a
irah rosete <irah.rosete@outlook.com>
2021-01-05 16:01:56 +1100

    crated default_grid method and the tests for it

--- tic-tac-toe/lib/board.rb
+++ tic-tac-toe/lib/board.rb
@@ -1,7 +1,13 @@
 module TicTacToe
   class Board
-    def initialize(input)
-      @grid = input.fetch(:grid)
+
+    attr_reader :grid
+    def initialize(input = {})
+      @grid = input.fetch(:grid, default_grid)
+    end
+
+    def default_grid
+      Array.new(3) {Array.new(3) {Cell.new}}
     end
   end
 end
--- tic-tac-toe/spec/board_spec.rb
+++ tic-tac-toe/spec/board_spec.rb
@@ -2,8 +2,27 @@ require_relative "../lib/board"
 
 module TicTacToe
   describe Board do
+
     it "initializes the board with a grid" do
       expect {Board.new(grid: "grid")}.to_not raise_error
     end
+
+    it "sets the grid with three rows by default" do
+      board = Board.new
+      expect(board.grid.size).to eq 3
+    end
+
+    it "sets each row of the grid with three cells by default" do
+      board = Board.new
+      board.grid.each do |row|
+        expect(row.size).to eq 3
+      end
+    end
+
+    it "returns the grid" do
+      board=Board.new(grid: "grid")
+      expect(board.grid).to eq "grid"
+    end
+
   end
 end
