613adad141b1477db35df6938bdba8f6f912dd7d
irah rosete <irah.rosete@outlook.com>
2021-01-05 22:25:18 +1100

    added all_same, any_empty and none_empty methods and tests

--- tic-tac-toe/spec/array_spec.rb
+++ tic-tac-toe/spec/array_spec.rb
@@ -1,13 +1,45 @@
 require_relative "../lib/array"
 
 describe Array do
-  it "returns true if all elements of the array is empty" do
-    expect(["", "", ""].all_empty?).to be_truthy
+  context "#all_empty?" do
+    it "returns true if all elements of the array is empty" do
+      expect(["", "", ""].all_empty?).to be_truthy
+    end
+    it "returns false if some elements of the array are not empty" do
+      expect(["", "X", ""].all_empty?).to be_falsey
+    end
+    it "returns true if the array is empty" do
+      expect([].all_empty?).to be_truthy
+    end
   end
-  it "returns false if some elements of the array are not empty" do
-    expect(["", "X", ""].all_empty?).to be_falsey
+
+  context "all_same?" do
+    it "returns true if all elements are the same" do
+      expect(["X", "X", "X"].all_same?).to be_truthy
+    end
+    it "returns false if all elements are not the same" do
+      expect(["X", "O", "X"].all_same?).to be_falsey
+    end
+    it "returns true if the array is empty" do
+      expect([].all_same?).to be_truthy
+    end
   end
-  it "returns true if the array is empty" do
-    expect([].all_empty?).to be_truthy
+
+  context "any_empty?" do
+    it "returns true if any element of the array is empty" do
+      expect(["", "X", ""].any_empty?).to be_truthy
+    end
+    it "returns false if all element of the array is not empty" do
+      expect(["X", "X", "O"].any_empty?).to be_falsey
+    end
+  end
+
+    context "none_empty?" do
+    it "returns false if any element of the array is empty" do
+      expect(["", "X", ""].none_empty?).to be_falsey
+    end
+    it "returns true if all element of the array is not empty" do
+      expect(["X", "X", "O"].none_empty?).to be_truthy
+    end
   end
 end
--- tic-tac-toe/spec/board_spec.rb
+++ tic-tac-toe/spec/board_spec.rb
@@ -3,31 +3,35 @@ require_relative "../lib/board"
 module TicTacToe
   describe Board do
 
-    it "initializes the board with a grid" do
-      expect{Board.new(grid: "grid")}.to_not raise_error
-    end
-
-    it "sets the grid with three rows by default" do
-      board = Board.new
-      expect(board.grid.size).to eq 3
-    end
-
-    it "sets each row of the grid with three cells by default" do
-      board = Board.new
-      board.grid.each do |row|
-        expect(row.size).to eq 3
+    context "#initialize" do
+      it "initializes the board with a grid" do
+        expect{Board.new(grid: "grid")}.to_not raise_error
       end
     end
 
-    it "returns the grid" do
-      board=Board.new(grid: "grid")
-      expect(board.grid).to eq "grid"
+    context "#default_grid" do
+      it "sets the grid with three rows by default" do
+        board = Board.new
+        expect(board.grid.size).to eq 3
+      end
+      it "sets each row of the grid with three cells by default" do
+        board = Board.new
+        board.grid.each do |row|
+          expect(row.size).to eq 3
+        end
+      end
+      it "returns the grid" do
+        board=Board.new(grid: "grid")
+        expect(board.grid).to eq "grid"
+      end
     end
 
-    it "returns cell based on the (x, y) coordinate" do
-      grid = [["", "", ""], ["X", "", ""], ["", "", ""]]
-      board = Board.new(grid: grid)
-      expect(board.get_cell(0,1)). to eq "X"
+    context "#get_cell" do
+      it "returns cell based on the (x, y) coordinate" do
+        grid = [["", "", ""], ["X", "", ""], ["", "", ""]]
+        board = Board.new(grid: grid)
+        expect(board.get_cell(0,1)). to eq "X"
+      end
     end
 
     # it "updates the value of the cell at (x, y) coordinate" do
@@ -38,25 +42,27 @@ module TicTacToe
     #   expect(board.get_cell(0, 1).value).to eq "O"
     # end
 
-    it "returns :winner if winner? is true" do
-      board = Board.new
-      allow(board).to receive(:winner?) {true}
-      # board.stub(:winner?) {true}
-      expect(board.game_over).to eq :winner
-    end
+    context "#game_over" do
+      it "returns :winner if winner? is true" do
+        board = Board.new
+        allow(board).to receive(:winner?) {true}
+        # board.stub(:winner?) {true}
+        expect(board.game_over).to eq :winner
+      end
 
-    it "returns :draw if winner? is false and draw? is true" do
-      board = Board.new
-      allow(board).to receive(:winner?) {false}
-      allow(board).to receive(:draw?) {true}
-      expect(board.game_over).to eq :draw
-    end
+      it "returns :draw if winner? is false and draw? is true" do
+        board = Board.new
+        allow(board).to receive(:winner?) {false}
+        allow(board).to receive(:draw?) {true}
+        expect(board.game_over).to eq :draw
+      end
 
-    it "keeps game on if winner? is false and draw? is false" do
-      board = Board.new
-      allow(board).to receive(:winner?) {false}
-      allow(board).to receive(:draw?) {false}
-      expect(board.game_over).to be_falsey
+      it "keeps game on if winner? is false and draw? is false" do
+        board = Board.new
+        allow(board).to receive(:winner?) {false}
+        allow(board).to receive(:draw?) {false}
+        expect(board.game_over).to be_falsey
+      end
     end
 
 
